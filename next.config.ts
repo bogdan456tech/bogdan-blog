ab850c436f2022df4a9292374e13a8ad273e2c60
Bogdan Arama <bogdan.arama@isa.utm.md>
2025-09-13 01:58:49 +0300

    Fix workflow install and add basePath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "export", // 👈 static export to ./out
+  output: "export",
+  basePath: "/bogdan-blog",     // 👈 important for subfolder deploy
+  assetPrefix: "/bogdan-blog/", // 👈 ensures CSS/JS load correctly
 };
 
 export default nextConfig;
