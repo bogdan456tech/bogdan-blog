21df13b35b020eb135032aa1a73a5269f8cb4669
Bogdan Arama <bogdan.arama@isa.utm.md>
2025-09-13 02:29:20 +0300

    Fix config for custom domain bogdanarama.com

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "export",
-  basePath: isProd ? "/bogdan-blog" : undefined,
-  assetPrefix: isProd ? "/bogdan-blog/" : undefined,
 };
 
 export default nextConfig;
