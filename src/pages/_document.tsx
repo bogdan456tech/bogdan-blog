44ad30d998fb31e1fcae93ec9093053aeafadc85
Bogdan Arama <bogdan.arama@isa.utm.md>
2025-09-13 03:14:19 +0300

    Add Google Analytics tracking

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,24 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-1ZPLGHRC8Y"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1ZPLGHRC8Y');
+            `,
+          }}
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
